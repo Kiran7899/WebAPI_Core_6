e7fa4987a6174552d84913fc6a787817016a876c
kiran7899@gmail.com <kiran7899@gmail.com>
2022-07-23 15:04:43 +0530

    GetPointOfInterest

---
+++ Cityinfo.API/Controllers/PointOfInterest.cs
@@ -0,0 +1,21 @@
+﻿using Cityinfo.API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cityinfo.API.Controllers
+{
+    [Route("api/cities/{cityid}/PointsOfInterest")]
+    [ApiController]
+    public class PointOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointOfInterest(int cityid)
+        {
+            var city = CitiInfoDataStore.current.Cities.FirstOrDefault(c => c.Id == cityid);
+            if(city == null)
+                return NotFound();
+
+            return Ok(city.pointOfInterest);
+        }
+    }
+}
