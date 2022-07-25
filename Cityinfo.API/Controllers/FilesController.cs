f02b860eae65e8f88cffc92d8f9a478c89377e61
kiran7899@gmail.com <kiran7899@gmail.com>
2022-07-25 07:55:08 +0530

    File Download - Incorrect format of file downloaded

---
+++ Cityinfo.API/Controllers/FilesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cityinfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet("{fileid}")]
+        public ActionResult GetFile(string fileid)
+        {
+            string fileName = "Kiran Kumar Balasubramanian Appointment Letter.pdf";
+
+            if(!System.IO.File.Exists(fileName))
+            {
+                return NotFound();
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(fileName);
+            return File(bytes,"text/plain",fileName);
+        }
+    }
+}
