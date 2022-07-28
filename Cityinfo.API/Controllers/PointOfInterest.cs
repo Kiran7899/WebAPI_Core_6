dd48a5e22cbf2a7a03422c4e51d9c298fdb85ebb
kiran7899@gmail.com <kiran7899@gmail.com>
2022-07-28 18:25:00 +0530

    Logging in console when city is not present for pointsofinterest

--- Cityinfo.API/Controllers/PointOfInterest.cs
+++ Cityinfo.API/Controllers/PointOfInterest.cs
@@ -8,12 +8,21 @@ namespace Cityinfo.API.Controllers
     [ApiController]
     public class PointOfInterestController : ControllerBase
     {
+
+        private readonly ILogger<PointOfInterestController> _logger;    
+        public PointOfInterestController(ILogger<PointOfInterestController> logger)
+        {
+            _logger = logger;
+        }
         [HttpGet]
         public ActionResult<IEnumerable<PointOfInterestDto>> GetPointOfInterest(int cityid)
         {
             var city = CitiInfoDataStore.current.Cities.FirstOrDefault(c => c.Id == cityid);
-            if(city == null)
+            if (city == null)
+            {
+                _logger.LogInformation("Not found");
                 return NotFound();
+            }
 
             return Ok(city.pointOfInterest);
         }
