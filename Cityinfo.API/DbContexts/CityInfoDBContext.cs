fd4a3458ef7f747f849c65e5ce09642c57c34473
kiran7899@gmail.com <kiran7899@gmail.com>
2022-08-02 15:32:08 +0530

    Created Migration and initial database using PM Console

--- Cityinfo.API/DbContexts/CityInfoDBContext.cs
+++ Cityinfo.API/DbContexts/CityInfoDBContext.cs
@@ -5,6 +5,11 @@ namespace Cityinfo.API.DbContexts
 {
     public class CityInfoDBContext : DbContext
     {
+        public CityInfoDBContext(DbContextOptions<CityInfoDBContext> options)
+            : base(options)
+        {
+
+        }
         public DbSet<City> Cities { get; set; } = null!; //Null forgiving operator
 
         public DbSet<PointOfInterest> PointOfInterests {get;set;} = null!;
