896a502c2b8c7f9eff8ca2b744daf80049b5d540
kiran7899@gmail.com <kiran7899@gmail.com>
2022-08-02 13:36:33 +0530

    Added EF Core Reference and Db Contexts

---
+++ Cityinfo.API/DbContexts/CityInfoDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Cityinfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cityinfo.API.DbContexts
+{
+    public class CityInfoDBContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } = null!; //Null forgiving operator
+
+        public DbSet<PointOfInterest> PointOfInterests {get;set;} = null!;
+    }
+}
