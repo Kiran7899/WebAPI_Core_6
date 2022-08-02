37949f79e55f2935b3cff13999b291ea2e550d8e
kiran7899@gmail.com <kiran7899@gmail.com>
2022-08-02 13:26:40 +0530

    Entity classes created. City, PoinitOfInterest

---
+++ Cityinfo.API/Entities/City.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cityinfo.API.Entities
+{
+    public class City
+    {
+        public City(string name)
+        {
+            Name = name;    
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }    
+        
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterests { get; set; } = new List<PointOfInterest>();
+    }
+}
---
+++ Cityinfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cityinfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City city { get; set; }  
+
+        public int CityId { get; set; }
+
+        public PointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
